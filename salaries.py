ad2999ba80afcb1eea6cb1828cad4c39284b5e3f
Jon Craton <jncraton@gmail.com>
2024-10-30 14:40:38 -0400

    Add more comments to handout code

--- salaries.py
+++ salaries.py
@@ -3,11 +3,21 @@ import csv
 with open("salaries.csv") as csvfile:
     rows = list(csv.reader(csvfile))
 
-    # Rows is now a list of lists containing the rows of the CSV
+    # `rows` is now a list of lists containing the rows of the CSV
 
     # For example
 
     # rows[0] is the first row of the file
     # rows[0][0] is the first value in the first row
 
-    # Complete the program so that it produces the expected output
+    # Compute and display the following items as detailed in the readme
+
+    # 1. The total number of salaries in the file (remember not to include the header row)
+
+    # 2. The average (mean) salary for entry-level positions
+
+    # 3. The average (mean) salary for entry-level positions with a Data Scientist Title
+
+    # 4. The highest salary for a fully remote, entry-level position
+
+    # 5. The average (mean) salary for all mid-level positions
